25d22886dfa1947717ee46d203d8fda93bf20515
Jimmy Pro <jimmyfam96@gmail.com>
2022-09-29 03:32:26 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      
+      
+      <Switch>
+      
+    <Route exact path='/'>
+    <Home/>
+    </Route>
+     <Route path='/stocks'>
+    <Stocks/>
+    </Route>
+     <Route path='/movies'>
+    <Movies/>
+    </Route>
+    
+    
+        <Route path >
+        <h1> Page not Found</h1>
+        </Route>
+    
+    </Switch>
+    
+    
     </div>
   );
 }
